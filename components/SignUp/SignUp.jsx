2703e396d065db2ea4251fd62a63d1276d2d0253
DigitalGenius-ui <miladmarketing282@gmail.com>
2023-08-12 21:41:45 +0800

    added

--- components/SignUp/SignUp.jsx
+++ components/SignUp/SignUp.jsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import Input from "../utils/Input";
-import { useFormik } from "formik";
-import * as yup from "yup";
 
 const SignUp = ({ setLog }) => {
   const inputs = [
@@ -33,51 +31,17 @@ const SignUp = ({ setLog }) => {
     },
   ];
 
-  const registerUser = () => {
-    alert(formik.values.password);
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-      rePassword: "",
-    },
-    onSubmit: registerUser,
-    validationSchema: yup.object().shape({
-      username: yup.string().required().min(4).max(10),
-      email: yup.string().required().email(),
-      password: yup
-        .string()
-        .required()
-        .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"),
-      rePassword: yup
-        .string()
-        .required()
-        .oneOf([yup.ref("password"), null], "Passwords Must Match"),
-    }),
-  });
-
-  const handleChange = (e) => {
-    const { target } = e;
-    formik.setFieldValue(target.name, target.value);
-  };
-
   return (
     <div
       className="border border-gray-600 p-5 rounded-md bg-white/10
       w-[80%] mx-auto md:w-[25rem]">
       <form
-        onSubmit={formik.handleSubmit}
         className="flex flex-col item-center gap-2">
         <h1 className="text-center text-xl uppercase pb-3">Sign Up</h1>
         {inputs.map((item, i) => (
           <Input
             item={item}
             key={i}
-            handleChange={handleChange}
-            formik={formik}
           />
         ))}
         <button
--- components/utils/Input.jsx
+++ components/utils/Input.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Input = ({ item, handleChange, formik }) => {
+const Input = ({ item }) => {
   const { type, placeholder, name, label } = item;
+  const error = false;
   return (
     <>
       <label className="text-xs">{label}</label>
@@ -9,16 +10,10 @@ const Input = ({ item, handleChange, formik }) => {
         type={type}
         placeholder={placeholder}
         name={name}
-        onChange={handleChange}
-        onBlur={formik.handleBlur}
-        className={`w-full p-3 outline-none bg-transparent text-xs
-        border border-gray-600 rounded-md focus:border-gray-300
-        ${formik.errors[name] ? "border-red-400 focus:border-red-400" : ""}
-        `}
+        className="w-full p-3 outline-none bg-transparent text-xs
+        border border-gray-600 rounded-md focus:border-gray-300"
       />
-      {formik.errors[name] && formik.touched[name] && (
-        <span className="text-xs text-red-400">{formik.errors[name]}</span>
-      )}
+      {error && <span className="text-xs text-red-400">Error Message</span>}
     </>
   );
 };
